bf8dee8db0ad6a96f47e145a1759c01f799ad8c2
Bo Feng <bfeng1216@gmail.com>
2016-01-17 23:33:33 -0600

    unindented

--- install.sh
+++ install.sh
@@ -2,42 +2,42 @@ OS_NAME=''
 GUI_MODE=0
 
 detect_OS() {
-  OS_NAME=`uname -s`
+    OS_NAME=`uname -s`
 }
 
 check_gui() {
-  if [ "$DISPLAY" ]; then
-    GUI_MODE=1
-  fi
+    if [ "$DISPLAY" ]; then
+        GUI_MODE=1
+    fi
 }
 
 patch_fonts() {
-  source 'fonts/install.sh'
+    source 'fonts/install.sh'
 }
 
 update_submodule() {
-  git submodule update --init --recursive
+    git submodule update --init --recursive
 }
 
 update_vim_bundle() {
-  echo "Install plugins"
-  vim -c "PluginInstall!" -cq
+    echo "Install plugins"
+    vim -c "PluginInstall!" -cq
 }
 
 link_config() {
-  vim="~/.vim"
-  echo "Backup existing configurations"
-  if [ -e $vim ]; then
-    if [ -d $vim ]; then
-      mv $vim ${vim}_bak
+    vim="~/.vim"
+    echo "Backup existing configurations"
+    if [ -e $vim ]; then
+        if [ -d $vim ]; then
+            mv $vim ${vim}_bak
+        fi
+        if [ -L $vim ]; then
+            rm $vim
+        fi
     fi
-    if [ -L $vim ]; then
-      rm $vim
-    fi
-  fi
-  echo "Link latest configurations"
-  ln -s ~/vim.config ~/.vim
-  ls -l ~/.vim
+    echo "Link latest configurations"
+    ln -s ~/vim.config ~/.vim
+    ls -l ~/.vim
 }
 
 detect_OS
